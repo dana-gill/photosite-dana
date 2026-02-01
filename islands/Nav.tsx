92b1034fed44f2e3ea52c727224c7241947512e2
dana-gill <danagslee@gmail.com>
2026-02-02 00:51:44 +0100

    Add name in Nav bar

--- islands/Nav.tsx
+++ islands/Nav.tsx
@@ -40,28 +40,33 @@ export default function Nav() {
     <>
       <nav class="w-full bg-transparent fixed top-0 left-0 z-50">
         <div class="px-4 py-4">
-          <ul class="flex gap-6 justify-end items-start">
-            {navLinks.map((link) => (
-              <li key={link.href}>
-                <a
-                  href={link.href}
-                  class="group text-gray-900 font-[500] transition duration-300"
-                >
-                  {link.label}
-                  <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-px bg-gray-900"></span>
-                </a>
-              </li>
-            ))}
+          <ul class="flex gap-6 justify-between items-start">
             <li>
-              <button
-                type="button"
-                onClick={handleWorkOpen}
-                class="group text-gray-900 font-[500] transition duration-300 cursor-pointer bg-transparent border-none p-0"
-              >
-                Work
-                <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-px bg-gray-900"></span>
-              </button>
+              <span class="text-gray-900 font-[500] italic">Dana Gill</span>
             </li>
+            <div class="flex gap-6">
+              {navLinks.map((link) => (
+                <li key={link.href}>
+                  <a
+                    href={link.href}
+                    class="group text-gray-900 font-[500] transition duration-300"
+                  >
+                    {link.label}
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-px bg-gray-900"></span>
+                  </a>
+                </li>
+              ))}
+              <li>
+                <button
+                  type="button"
+                  onClick={handleWorkOpen}
+                  class="group text-gray-900 font-[500] transition duration-300 cursor-pointer bg-transparent border-none p-0"
+                >
+                  Work
+                  <span class="block max-w-0 group-hover:max-w-full transition-all duration-300 h-px bg-gray-900"></span>
+                </button>
+              </li>
+            </div>
           </ul>
         </div>
       </nav>
