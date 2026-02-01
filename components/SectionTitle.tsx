8ec8f12479c54538015675813480e2c80d45a7cd
dana-gill <danagslee@gmail.com>
2026-02-01 22:53:35 +0100

    Use 200 in SectionTitle

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -2,7 +2,7 @@ import type { SectionTitleProps } from "../types/section-title.ts";
 
 export const SectionTitle = ({ children }: SectionTitleProps) => {
   return (
-    <h1 class="text-4xl font-light mb-8 text-center">
+    <h1 class="text-4xl font-[200] italic mb-8 text-center">
       {children}
     </h1>
   );
