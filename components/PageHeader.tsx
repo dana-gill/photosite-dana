613c5aaf960b8a26939e4497b9311745b6767510
dana-gill <danagslee@gmail.com>
2026-02-03 22:37:44 +0100

    Use text-lg for bigger devices for PageHeader

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -7,7 +7,7 @@ export const PageHeader = ({ subtitle, title }: PageHeaderProps) => {
         <h1 class="text-4xl font-[200] italic mt-8 mb-2 text-center fade-in-title">
           {title}
         </h1>
-        <p class="text-sm font-[200] text-center">
+        <p class="text-sm md:text-lg font-[200] text-center">
           {subtitle}
         </p>
       </div>
