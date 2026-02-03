a833662812472bc9324f50d87f6b33d9fb47cb03
dana-gill <danagslee@gmail.com>
2026-02-03 22:13:20 +0100

    Align the Carousel to center of page

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,11 +3,8 @@ import { LandingPageTitle } from "../components/LandingPageTitle.tsx";
 
 export default function Home() {
   return (
-    <div class="min-h-screen bg-gray-50">
-      {/* <div class="pt-20 pb-4 py-8 flex items-center justify-center">
-        <LandingPageTitle>Dana Gill Photography</LandingPageTitle>
-      </div> */}
-      <div class="px-4 py-5">
+    <div class="min-h-screen bg-gray-50 flex items-center justify-center">
+      <div class="w-full px-4">
         <Carousel />
       </div>
     </div>
