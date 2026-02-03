66b4eecb22c42f90f59023e7efeb0d47e2b238f9
dana-gill <danagslee@gmail.com>
2026-02-03 23:13:05 +0100

    Adapt carousel to mobile

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,8 +2,8 @@ import Carousel from "../islands/Carousel.tsx";
 
 export default function Home() {
   return (
-    <div class="min-h-screen bg-gray-50 flex justify-center pt-20">
-      <div class="w-full px-4">
+    <div class="min-h-screen bg-gray-50 flex justify-center pt-20 md:items-center">
+      <div class="w-full max-w-screen-xl px-4">
         <Carousel />
       </div>
     </div>
