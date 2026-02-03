c9430c15e8af37ee2c97eecb24820f3b85ffaa0b
dana-gill <danagslee@gmail.com>
2026-02-03 22:38:23 +0100

    Center the carousel on y-axis

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ import Carousel from "../islands/Carousel.tsx";
 
 export default function Home() {
   return (
-    <div class="min-h-screen bg-gray-50 flex items-center justify-center">
+    <div class="min-h-screen bg-gray-50 flex justify-center pt-20">
       <div class="w-full px-4">
         <Carousel />
       </div>
