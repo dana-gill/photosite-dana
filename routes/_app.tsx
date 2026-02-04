9ebb27ebfd1fa88ca4f2925c6645d63503511015
dana-gill <danagslee@gmail.com>
2026-02-04 19:01:00 +0100

    Add more og metadata

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -12,9 +12,16 @@ export default define.page(function App({ Component }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Dana Gill Photography</title>
+        <meta property="og:title" content="Dana Gill Photography" />
+        <meta property="og:description" content="Dana Gill is a Filipino engineer and hobby-artist based in Berlin, capturing portraits and moments from travels and life with analog film." />
+        <meta property="og:image" content="https://reassuring-peace-c7bac71a31.media.strapiapp.com/about_3_a193280a6a.jpg" />
+        <meta property="og:url" content="https://www.danagill.photography" />
         <meta property="og:site_name" content="Dana Gill Photography" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Dana Gill Photography" />
+        <meta name="twitter:description" content="Dana Gill is a Filipino engineer and hobby-artist based in Berlin, capturing portraits and moments from travels and life with analog film." />
+        <meta name="twitter:image" content="https://reassuring-peace-c7bac71a31.media.strapiapp.com/about_3_a193280a6a.jpg" />
         <link rel="icon" type="image/jpeg" href="/favicon.jpg" />
       </head>
       <body>
