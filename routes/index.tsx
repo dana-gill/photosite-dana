3d6893db51e26e29efeaa4d89939929b09c0d636
dana-gill <danagslee@gmail.com>
2026-02-07 18:49:53 +0100

    Use --preview images in carousel

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,19 +1,14 @@
 import { define } from "../utils.ts";
 import { getAllImages } from "../services/cache-manager.ts";
-import type { StrapiImage } from "../types/strapi.ts";
 import CarouselWrapper from "../islands/CarouselWrapper.tsx";
 
 export const handler = define.handlers({
   GET: async (ctx) => {
     const allImages = await getAllImages(ctx.state.kv);
-    const shuffledImages = [...allImages]
-      .filter((image) => !image.name.includes("about"))
-      .sort(() => Math.random() - 0.5);
-    const randomImages: ReadonlyArray<StrapiImage> = shuffledImages.slice(
-      0,
-      10,
-    );
-    return { data: randomImages };
+    const previewImages = allImages
+      .filter((image) => image.name.includes("--preview"))
+      .slice(0, 10);
+    return { data: previewImages };
   },
 });
 
